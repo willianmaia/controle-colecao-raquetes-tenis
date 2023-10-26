924d85a7432660791734e01764e3a08e6a5ed0b9
Willian Maia <willian.maia@sysmap.com.br>
2023-10-26 19:01:46 -0300

    adicionado validacao expressoes regulares

--- src/app/components/cadastrar/cadastrar.component.ts
+++ src/app/components/cadastrar/cadastrar.component.ts
@@ -14,10 +14,10 @@ export class CadastrarComponent {
 
   constructor(private router: Router, private raqueteService: RaqueteService, private fb: FormBuilder) {
     this.raqueteForm = this.fb.group({
-      nomeMarca: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(300)]],
+      nomeMarca: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(300), Validators.pattern(/^[a-zA-Z0-9\s]*$/)]],
       valor: ['',[Validators.required, Validators.pattern(/^\d{1,10}(\,\d{2})?$/),],],
-      link: ['', [Validators.required, Validators.maxLength(300),]],
-      descricao: ['', [Validators.required, Validators.maxLength(300)]]
+      link: ['', [Validators.required, Validators.maxLength(300),Validators.pattern(/^https?:\/\/\S+/)]],
+      descricao: ['', [Validators.required, Validators.maxLength(300),Validators.pattern(/^[a-zA-Z0-9\s@!#$%^&*()_+.,;?]+$/)]]
     });
   }
 
